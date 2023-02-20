40716dd3dab72a2bd15c2adbb5426348b712985e
이맘비 <80617446+hanbi625@users.noreply.github.com>
2023-02-20 21:19:10 +0900

    Add files via upload

---
+++ 4장 수식과 연산자 실습문제/4-3.c
@@ -0,0 +1,14 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+#pragma warning(disable:4996)
+#include <stdio.h>
+
+int main()
+{
+	int time;
+	scanf("%d", &time);
+
+	printf("%d:%d:%d", time / 3600, time % 3600 / 60, time % 60);
+
+	return 0;
+}
