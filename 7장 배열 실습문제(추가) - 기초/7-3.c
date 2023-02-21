097062aa512c489617cdc27ab79cb77b12f2384d
이맘비 <80617446+hanbi625@users.noreply.github.com>
2023-02-21 17:44:33 +0900

    Create 7-3.c

---
+++ 7장 배열 실습문제(추가) - 기초/7-3.c
@@ -0,0 +1,15 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+int main() {
+	int x[10], y[10], z[10], k;
+	for (k = 0; k < 10; k++)
+		scanf("%d", &x[k]);
+	for (k = 0; k < 10; k++)
+		scanf("%d", &y[k]);
+	for (k = 0; k < 10; k++) {
+		z[k] = x[k] + y[k];
+		printf(" %d", z[k]);
+	}
+
+	return 0;
+}
