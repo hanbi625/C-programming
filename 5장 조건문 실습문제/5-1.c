35ad590cb285df54773985d3a3247b49f1ff10e1
이맘비 <80617446+hanbi625@users.noreply.github.com>
2023-02-21 09:53:25 +0900

    Update 5-1.c

--- 5장 조건문 실습문제/5-1.c
+++ 5장 조건문 실습문제/5-1.c
@@ -1,1 +1,16 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+int main()
+{
+	int N;
+	scanf("%d", &N);
+	if (N < 0)
+		printf("cold, indoor\n");
 
+	else if (N < 40)
+		printf("moderate, outdoor\n");
+	else
+		printf("hot, indoor\n");
+
+	return 0;
+}
