e9964cf5f2d90ed4536fbd86bb3133456c6333e5
이맘비 <80617446+hanbi625@users.noreply.github.com>
2023-02-21 13:07:06 +0900

    Create 6-14.c

---
+++ 6장 반복문 온라인 노트 (예제) 문제/6-14.c
@@ -0,0 +1,13 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+int main() {
+    int i, j, N;
+    scanf("%d", &N);
+    for (i = N; i >= 1; --i) {
+        for (j = 0; j < i; ++j)
+            printf("*");
+        printf("\n");
+    }
+
+    return 0;
+}
