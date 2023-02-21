3adfe8c47254d7a18cf9fd04870e571ccc6cf789
이맘비 <80617446+hanbi625@users.noreply.github.com>
2023-02-21 17:33:40 +0900

    Create 6-6(2).c

---
+++ 6장 반복문 실습문제/6-6(2).c
@@ -0,0 +1,17 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+int main() {
+	int N, i, j;
+	int num = 1;
+
+	scanf("%d", &N);
+	for (i = 1; i <= N; i++) {
+		for (j = 1; j <= N; j++) {
+			printf(" %d", num % 10);
+			num++;
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
